9a512502a2092ae29effb948b5b97070c9d78bf0
Logodum <15616120+PavelProvotorov@users.noreply.github.com>
2023-06-22 20:01:33 +0300

    Final cleanup and full game loop - Added checkInput() function to validate player input - Computed input is now randomized as it should - Added final message on game end

--- index.js
+++ index.js
@@ -2,17 +2,30 @@ console.log("Script started...")
 
 const choice_list = ["rock", "paper", "scissors"]
 let player_choice = ""
-let computer_choice = getRandomChoice()
+let computer_choice = ""
 let player_score = 0
 let computer_score = 0
+let max_score = 5
 
 function getPlayerChoice(){
-    return prompt("Please enter 'rock', 'paper' or 'scissors': ", getRandomChoice())
+    return prompt("Please enter 'rock', 'paper' or 'scissors': ")
 };
 
 function getRandomChoice(){
     return choice_list[Math.floor(Math.random() * choice_list.length)]
 };
+
+function checkInput(input){
+    if (typeof(input) === "string"){
+        player_choice = input
+            .toLowerCase()
+            .trim()
+        if(choice_list.includes(player_choice)){
+            return true
+        }
+    }
+    return false
+}
 
 function playRound(option_a, option_b){
     if (option_a == option_b){
@@ -49,7 +62,24 @@ function playRound(option_a, option_b){
     }
 };
 
-while (player_score < 5 && computer_score < 5){
-    player_choice = getPlayerChoice()
+// Game start
+while (player_score < max_score && computer_score < max_score){
+    // Reset choices to default
+    computer_choice = getRandomChoice()
+    player_choice = ""
+
+    // Request input until valid
+    while (checkInput(player_choice) == false){
+        player_choice = getPlayerChoice()
+    };
+
+    // Display scorecard
     alert("Result: " + playRound(player_choice,computer_choice) + "\n\nPlayer choice: " + player_choice +  "\nComputer choice: " + computer_choice + "\n\nPlayer score: " + player_score + "\nComputer score: " + computer_score)
+};
+
+// Game end
+if (player_score == 5){
+    alert("YOU WON :) \n\nREFRESH PAGE TO PLAY AGAIN")
+} else {
+    alert("YOU LOST :( \n\nREFRESH PAGE TO PLAY AGAIN")
 };
