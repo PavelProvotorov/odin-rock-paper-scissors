b1c785e4ad74d50c0c5e0dc39b539fba4c96722e
Logodum <15616120+PavelProvotorov@users.noreply.github.com>
2023-07-19 20:16:00 +0300

    Revamped round handling - Remade playRound() function - Remade compareOptions() function - Rock, Paper & Scissors are now stored as Objects, instead of string - Removed unused functions and while loop - Other minor changes

--- index.js
+++ index.js
@@ -6,100 +6,82 @@ const BUTTON_PAPER = document.getElementById("paper")
 const BUTTON_SCISSORS = document.getElementById("scissors")
 const TEXT_OUTPUT = document.getElementById("output")
 
+const ROCK = {
+    name: "ROCK",
+    beats: [
+        "SCISSORS"
+    ]
+};
+
+const PAPER = {
+    name: "PAPER",
+    beats: [
+        "ROCK"
+    ]
+};
+const SCISSORS = {
+    name: "SCISSORS",
+    beats: [
+        "PAPER"
+    ]
+};
+
+const OPTION_LIST = [ROCK, PAPER, SCISSORS]
+
+let player_score = 0
+let player_choice = []
+
+let computer_score = 0
+let computer_choice = []
+
+let max_score = 5
+
+// FUNCTIONS
+function playRound(option_a, option_b){
+    if (player_score < max_score && computer_score < max_score){
+        console.log(option_a)
+        console.log(option_b)
+        compareOptions(option_a, option_b)
+    } else {
+        console.log("GAME OVER")
+    }
+};
+
+function compareOptions(option_a, option_b){
+    const outcome = option_a.beats.includes(option_b.name)
+    if (option_a === option_b){
+        console.log("DRAW")
+        return
+    } else if (outcome === true) {
+        console.log("PLAYER WON")
+        return
+    } else if (outcome === false) {
+        console.log("COMPUTER WON")
+        return
+    } else {
+        console.log("ERROR")
+    };
+};
+
+function getRandomOption(){
+    return OPTION_LIST[Math.floor(Math.random() * OPTION_LIST.length)]
+};
 
 // SIGNALS
 BUTTON_ROCK.onclick = (event) => {
-    TEXT_OUTPUT.textContent = "ROCK"
+    player_choice = ROCK
+    computer_choice = getRandomOption()
+    playRound(player_choice, computer_choice)
 };
 
 BUTTON_PAPER.onclick = (event) => {
-    TEXT_OUTPUT.textContent = "PAPER"
+    player_choice = PAPER
+    computer_choice = getRandomOption()
+    playRound(player_choice, computer_choice)
 };
 
 BUTTON_SCISSORS.onclick = (event) => {
-    TEXT_OUTPUT.textContent = "SCISSORS"
-};
-
-const choice_list = ["rock", "paper", "scissors"]
-let player_choice = ""
-let computer_choice = ""
-let player_score = 0
-let computer_score = 0
-let max_score = 0
-
-function getPlayerChoice(){
-    return prompt("Please enter 'rock', 'paper' or 'scissors': ")
-};
-
-function getRandomChoice(){
-    return choice_list[Math.floor(Math.random() * choice_list.length)]
-};
-
-function checkInput(input){
-    if (typeof(input) === "string"){
-        player_choice = input
-            .toLowerCase()
-            .trim()
-        if(choice_list.includes(player_choice)){
-            return true
-        }
-    }
-    return false
-}
-
-function playRound(option_a, option_b){
-    if (option_a == option_b){
-        return "Tie!"
-    } else {
-        switch (option_a + " vs " + option_b){
-            // Rock
-            case "rock vs paper":
-                computer_score++
-                return "Computer Won!"
-
-            case "rock vs scissors":
-                player_score++
-                return "Player Won!"
-
-            // Paper
-            case "paper vs rock":
-                player_score++
-                return "Player Won!"
-
-            case "paper vs scissors":
-                computer_score++
-                return "Computer Won!"
-
-            // Scissors
-            case "scissors vs rock":
-                computer_score++
-                return "Computer Won!"
-
-            case "scissors vs paper":
-                player_score++
-                return "Player Won!"
-        }
-    }
-};
-
-// Game start
-while (player_score < max_score && computer_score < max_score){
-    // Reset choices to default
-    computer_choice = getRandomChoice()
-    player_choice = ""
-
-    // Request input until valid
-    while (checkInput(player_choice) == false){
-        player_choice = getPlayerChoice()
-    };
-
-    // Display scorecard
-    alert("Result: " + playRound(player_choice,computer_choice) + "\n\nPlayer choice: " + player_choice +  "\nComputer choice: " + computer_choice + "\n\nPlayer score: " + player_score + "\nComputer score: " + computer_score)
-};
-
-// Game end
-if (player_score == 5){
-    alert("YOU WON :) \n\nREFRESH PAGE TO PLAY AGAIN")
-} else {
-    //alert("YOU LOST :( \n\nREFRESH PAGE TO PLAY AGAIN")
+    player_choice = SCISSORS
+    computer_choice = getRandomOption()
+    playRound(player_choice, computer_choice)
 };
